23222c7dfc028613a6525fff5256b44ce66149ff
rahulranjan94 <rahulranjan.shree@gmail.com>
2025-12-14 21:11:18 +0530

    Basis Streamlit

---
+++ intro.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("My Streamlit App")
+st.subheader("Deploying using :blue[Streamlit Cloud]")
+st.write("We are learning streamlit for the first time")
---
+++ mlops_env/Scripts/intro.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("My Streamlit App")
+st.subheader("Deploying using :blue[Streamlit Cloud] ")
