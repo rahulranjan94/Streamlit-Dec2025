51c4ff1c4c86ae2c5ad8eb60f8b829eecfcb3582
rahulranjan94 <rahulranjan.shree@gmail.com>
2025-12-14 21:22:56 +0530

    add interactive elements

--- intro.py
+++ intro.py
@@ -3,3 +3,17 @@ import streamlit as st
 st.title("My Streamlit App")
 st.subheader("Deploying using :blue[Streamlit Cloud]")
 st.write("We are learning streamlit for the first time")
+agree = st.checkbox("I agree with Rahul ")
+
+if agree:
+    st.write("You are great!")
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    ["Comedy","Drama", "Documentary"],
+)
+
+if genre == "Comedy":
+    st.write("You comedy me ... Hahaha!!")
+elif genre == "Drama":
+    st.write("You drama me ... Boohoo!")
